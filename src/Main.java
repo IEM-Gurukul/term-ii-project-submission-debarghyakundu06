f4ca4e00e44ec53f6e437ba8eac11f76c7bd0420
Debarghya Kundu <debarghyakundu007@gmail.com>
2026-04-06 17:09:15 +0530

    Handled exceptions using try-catch in main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Library Management System Started");
+
+        Library lib = new Library();
+
+        try {
+            lib.issueBook(1);
+            lib.returnBook(1);
+        } catch (LibraryException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
