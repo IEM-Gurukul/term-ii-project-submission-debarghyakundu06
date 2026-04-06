173b6aaf413985c00627739d1379313554e56282
Debarghya Kundu <debarghyakundu007@gmail.com>
2026-04-06 17:46:46 +0530

    Changed displaybooks function in library.java and implemented sample data in main method

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,14 @@ public class Main {
         Library lib = new Library();
         Member m1 = new StudentMember(9081, "Debarghya", "CSE-IOT");
         m1.displayMember();
+        
+        Book b1 = new Book(1, "Java Basics", "Author A");
+        Book b2 = new Book(2, "OOP Concepts", "Author B");
+
+        lib.addBook(b1);
+        lib.addBook(b2);
+
+        lib.displayBooks();
 
         try {
             lib.issueBook(1);
